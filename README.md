9ea081127982ea5888f2ea9db33c2e926a5acfc3
Mathieu RENE <mathieu.rene@am-creations.fr>
2013-10-09 13:52:00 +0200

    installation procedure assets (js and css) in one line also

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ razor-shrink
 ============
 Shrink Html at render time of ASP.net MVC4 application. 
 It removes unnecessary space and carriage returns, saving up to ±25% of bandwidth.
+
+Installation
+============
+Change pageBaseType of the web.config file int the Views directory of our project
+ie:
+<system.web.webPages.razor>
+    <host factoryType="System.Web.Mvc.MvcWebRazorHostFactory, System.Web.Mvc, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31BF3856AD364E35" />
+    <!--<pages pageBaseType="System.Web.Mvc.WebViewPage">-->
+    <pages pageBaseType="RazorShrink.WebViewPageShrink">
+      <namespaces>
+        <add namespace="System.Web.Mvc" />
+        <add namespace="System.Web.Mvc.Ajax" />
+        <add namespace="System.Web.Mvc.Html" />
+        <add namespace="System.Web.Optimization"/>
+        <add namespace="System.Web.Routing" />        
+      </namespaces>
+    </pages>
+  </system.web.webPages.razor>
--- RazorShrink/WebViewPageShrink.cs
+++ RazorShrink/WebViewPageShrink.cs
@@ -1,16 +1,26 @@
 ﻿using System.Text.RegularExpressions;
+using System.Web;
 using System.Web.Mvc;
 
 namespace RazorShrink
 {
-    public abstract class WebViewPageShrink<T> : WebViewPageShrink
-    {
-    }
-
-    public abstract class WebViewPageShrink : WebViewPage
+    public abstract class WebViewPageShrink<T> : WebViewPage<T>
     {
         private static readonly Regex RegexEmptyLines = new Regex(@"\s*\n\s*", RegexOptions.Compiled | RegexOptions.Multiline);
         private static readonly Regex RegexOverSpace = new Regex(@"\s{2,}", RegexOptions.Compiled | RegexOptions.Multiline);
+
+        public override void Write(object value)
+        {
+            if (value != null && !IsDebuggingEnabled() && value is HtmlString)
+            {
+                var v = ((HtmlString) value).ToHtmlString();
+                v = RegexEmptyLines.Replace(v, string.Empty);
+                v = RegexOverSpace.Replace(v, " ");
+                value = new HtmlString(v);
+            }
+
+            base.Write(value);
+        }
 
         public override void WriteLiteral(object value)
         {
@@ -27,7 +37,7 @@ namespace RazorShrink
         /// </summary>
         private static bool IsDebuggingEnabled()
         {
-#if TEST_WHITESPACE_CLEANER
+#if TEST_RAZORSHRINK
             return false;
 #endif
             return
