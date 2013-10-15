e769471a4a9ccff11172deec8f5ebbb239956eb5
Mathieu RENE <mathieu.rene@am-creations.fr>
2013-10-15 21:12:22 +0200

    unit test project initialization

---
+++ RazorShrink.xUnit/HtmlShrinkerTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace RazorShrink.xUnit
+{
+
+    public class HtmlShrinkerTest
+    {
+        [Fact]
+        public void TestRegexShrink()
+        {
+            var s = new HtmlShrinker();
+            Assert.Equal("", "");
+        }
+    }
+}
