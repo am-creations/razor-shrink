e64668af971a093d4caa92bc9d301529d69288b3
Mathieu RENE <mathieu.rene@am-creations.fr>
2013-10-16 18:26:04 +0200

    initialize some unit test file

--- RazorShrink.xUnit/HtmlShrinkerTest.cs
+++ RazorShrink.xUnit/HtmlShrinkerTest.cs
@@ -1,15 +1,27 @@
-﻿using Xunit;
+﻿using System.IO;
+using Xunit;
 
 namespace RazorShrink.xUnit
 {
-
     public class HtmlShrinkerTest
     {
+        const string ActualDirectory = "Actual";
+        const string ExpectedDirectory = "Expected";
+
         [Fact]
         public void TestRegexShrink()
         {
             var s = new HtmlShrinker();
-            Assert.Equal("", "");
+
+            var di = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory().Replace("\\bin\\Debug", ""), ActualDirectory));
+            foreach (var f in di.GetFiles("*.html"))
+            {
+                var actualContent = File.ReadAllText(f.FullName);
+                var expectedContent = File.ReadAllText(f.FullName.Replace(ActualDirectory, ExpectedDirectory));
+
+                var strB = s.Shrink(actualContent);
+                Assert.Equal(0, string.CompareOrdinal(expectedContent, strB));
+            }
         }
     }
 }
